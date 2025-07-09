92cef358bbf673d02231c059b19a0549a4cfe32c
fabiola <20230033@ricaldone.edu.sv>
2025-07-08 22:38:03 -0600

    documentacion

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import ClienteRoutes from "./src/Routes/Cliente.js";
 import ReservaRoutes from "./src/Routes/Reserva.js";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+
 
 const app = express();
 
@@ -10,5 +14,13 @@ app.use(cookieParser());
 
 app.use("/api/Clientes", ClienteRoutes);
 app.use("/api/Reserva", ReservaRoutes);
+
+//Traemos el archivo json
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.resolve("./PartPlus.json"), "utf-8")
+);
+ 
+//Mostramos el archivo al ingresar a /api/docs
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
